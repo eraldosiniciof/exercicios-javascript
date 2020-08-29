7f5405f0f3cc71c6d7998051c22817de89f1ff9c
eraldosiniciof <eraldosinicio@gmail.com>
2020-08-29 18:40:57 -0300

    adicionando soluçoes

---
+++ exec1.js
@@ -0,0 +1,20 @@
+const resultado = document.querySelector('#resultados')
+
+// Função criada em um ficheiro externo que mostra uma mensagem
+
+function mensagem() {
+    open('exec1.html', '_blank')
+}
+
+// aparece um número aleatório entre 0 e 50
+function gerarNumero() {
+    let gerar = Math.round(Math.random() * 100, 0)
+    let verificar = gerar >= 0 && gerar <= 50
+
+    while (!verificar) {
+        gerar = Math.round(Math.random() * 100, 0)
+        verificar = gerar >= 0 && gerar <= 50
+    }
+    
+    resultado.innerHTML = `Número aleatório entre 0 e 50:<br>${gerar}`
+}
